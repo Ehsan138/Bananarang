ed5a98f04caeae6a7a3fb2362a7e53cd9df78534
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2023-04-04 13:22:53 -0700

    Update the Main file

--- src/main/java/com/example/bananarang/Main.java
+++ src/main/java/com/example/bananarang/Main.java
@@ -19,5 +19,10 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void stop() throws Exception {
+        System.exit(0);
+    }
 }
 
