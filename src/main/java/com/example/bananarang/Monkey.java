221ae69217816029a100137c03cd0aeebe1ddcf5
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2023-04-04 13:23:27 -0700

    Update the Monkey class

--- src/main/java/com/example/bananarang/Monkey.java
+++ src/main/java/com/example/bananarang/Monkey.java
@@ -1,28 +1,14 @@
 package com.example.bananarang;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 
 public class Monkey extends GameObject {
 
     private boolean moveLeft, moveRight;
-    private Banana banana;
-    private final double moveSpeed = 3;
-
-    private Image bananaImage;
+    private final double moveSpeed = 20;
 
     public Monkey(GraphicsContext gc, double x, double y) {
-        super(gc, "C:\\Users\\ehsan\\IdeaProjects\\Bananarang\\src\\main\\resources\\monkey.jpg", x, y, 100, 100);
-        this.bananaImage = new Image("C:\\Users\\ehsan\\IdeaProjects\\Bananarang\\src\\main\\resources\\banana.jpg");
-    }
-
-    @Override
-    public void draw() {
-        gc.drawImage(image, x, y, width, height);
-        gc.drawImage(bananaImage, x + 50, y + 20, 30, 30); // Adjust these values to position the banana correctly
-        if (banana != null) {
-            banana.draw();
-        }
+        super(gc, "C:\\Users\\ehsan\\IdeaProjects\\Bananarang\\src\\main\\resources\\monkey.png", x, y, 100, 100);
     }
 
     public void update() {
@@ -31,15 +17,6 @@ public class Monkey extends GameObject {
         }
         if (moveRight && x < gc.getCanvas().getWidth() - width) {
             x += moveSpeed;
-        }
-        if (banana != null) {
-            banana.update();
-        }
-    }
-
-    public void throwBanana() {
-        if (banana == null || !banana.isVisible()) {
-            banana = new Banana(gc, x + width / 2, y);
         }
     }
 
@@ -50,10 +27,4 @@ public class Monkey extends GameObject {
     public void setMoveRight(boolean moveRight) {
         this.moveRight = moveRight;
     }
-
-    public Banana getBanana() {
-        return banana;
-    }
 }
-
-
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.bananarang {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.bananarang to javafx.fxml;
